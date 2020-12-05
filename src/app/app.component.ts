401a6c8af114283ea33a556fcf3d4615ff1b9031
Yegor Dargin <darginyegor@gmail.com>
2020-12-05 19:01:25 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'anb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'alice-n-bob';
+}
