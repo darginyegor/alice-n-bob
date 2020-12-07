83a9daafa0e4152f8ef28130ca20f02e70d64358
Yegor Dargin <darginyegor@gmail.com>
2020-12-07 15:14:15 +0300

    remove example title tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  // title = 'alice-n-bob';
-}
+export class AppComponent { }
